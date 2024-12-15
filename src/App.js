d12fc4e1f8e3c8b2233a62b28c12206ece5a08a0
cleyton21 <cfernando_21@hotmail.com>
2024-12-15 18:06:25 -0300

    traalhando com props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = "Cleyton"
-  const newName = name.toUpperCase()
+  const nome = "Cleyton"
+  const newName = nome.toUpperCase()
 
   const url = "https:///via.placeholder.com/150"
 
@@ -13,9 +15,11 @@ function App() {
      <h1>Olá Mundo!</h1>
      <h2>Estou aprendendo React</h2>
      <p>Meu nome é {newName}</p>
-     <img src={url} alt={name} />
+     <img src={url} alt={nome} />
 
      <HelloWorld /> 
+     <SayMyName nome={nome} />
+     <Pessoa nome="Cleyton" idade={23} profissao="Desenvolvedor" />
      </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props) {
+    return (
+        <div>
+            <h1>Meu nome é {props.nome}</h1>
+        </div>
+    );
+}
+
+export default SayMyName
